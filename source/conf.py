7a236b01eda21eddb63daf7341e824aa3e745bca
Wellington Uemura <wellingtonuemura@gmail.com>
2019-08-05 05:36:33 -0300

    Arrumando o tema

--- source/conf.py
+++ source/conf.py
@@ -193,6 +193,8 @@ html_show_sourcelink = False
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'MAME'
 
+# Read the docs
+html_style=""
 
 # -- Options for LaTeX output --------------------------------------------------
 
